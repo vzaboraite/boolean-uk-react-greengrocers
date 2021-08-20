747c16e49ed89afeaeabc4928846ddcb8f87f9ce
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-08-20 16:25:16 +0100

    Create `initialStoreItems[]` and `userCart[]`; set created arrays in `state`.

--- src/App.js
+++ src/App.js
@@ -12,10 +12,74 @@ Here's what a store item should look like
 What should a cart item look like? 🤔
 */
 
-const initialStoreItems = [];
+const initialStoreItems = [
+  {
+    id: "001-beetroot",
+    name: "beetroot",
+    price: 0.35,
+  },
+  {
+    id: "002-carrot",
+    name: "carrot",
+    price: 0.35,
+  },
+  {
+    id: "003-apple",
+    name: "apple",
+    price: 0.35,
+  },
+  {
+    id: "004-apricot",
+    name: "apricot",
+    price: 0.35,
+  },
+  {
+    id: "005-avocado",
+    name: "avocado",
+    price: 0.35,
+  },
+  {
+    id: "006-bananas",
+    name: "bananas",
+    price: 0.35,
+  },
+  {
+    id: "007-bell-pepper",
+    name: "bell pepper",
+    price: 0.35,
+  },
+  {
+    id: "008-berry",
+    name: "berry",
+    price: 0.35,
+  },
+  {
+    id: "009-blueberry",
+    name: "blueberry",
+    price: 0.35,
+  },
+  {
+    id: "010-eggplant",
+    name: "eggplant",
+    price: 0.35,
+  },
+];
+
+const userCart = [
+  {
+    item: {
+      id: "001-beetroot",
+      name: "beetroot",
+      price: 0.35,
+    },
+    quantity: 1,
+  },
+];
 
 export default function App() {
-  // Setup state here...
+  const [storeItems, setStoreItems] = useState(initialStoreItems);
+  const [cartItems, setCartItems] = useState(userCart);
+
 
   return (
     <>
