d4e3b9a4448cc86f27cedd6622d84d367abffc8c
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-08-23 17:00:26 +0100

    Allow filter by type.

--- src/components/Store/index.js
+++ src/components/Store/index.js
@@ -1,11 +1,37 @@
+import { useState } from "react";
+import FilterOptions from "./components/FilterOptions";
 import StoreItemList from "./components/StoreItemList";
 
 function Store(props) {
   const { storeItems, addItemToCart } = props;
+  const [type, setType] = useState("");
+
+  /* Filter functions */
+
+  const filterByType = (storeItems) => {
+    const filteredItems = storeItems.filter((item) => type === item.type);
+    return filteredItems;
+  };
+
+  const getItemsToRender = (storeItems) => {
+    let result = storeItems;
+    if (type !== "") {
+      result = filterByType(storeItems);
+    }
+    return result;
+  };
+
+  const itemsToRender = getItemsToRender(storeItems);
+
+  const handleTypeChange = (event) => setType(event.target.value);
+
   return (
     <header id="store">
       <h1>Greengrocers</h1>
-      <StoreItemList storeItems={storeItems} addItemToCart={addItemToCart} />
+      <form>
+        <FilterOptions onChange={handleTypeChange} />
+      </form>
+      <StoreItemList storeItems={itemsToRender} addItemToCart={addItemToCart} />
     </header>
   );
 }
